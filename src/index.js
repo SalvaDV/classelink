41b598e7d4a5a9d087c57009ef4447d4549e0836
SalvaDV <111193994+SalvaDV@users.noreply.github.com>
2026-04-06 19:11:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "TU_DSN_AQUI",
+  environment: process.env.NODE_ENV,
+  enabled: process.env.NODE_ENV === "production",
+  tracesSampleRate: 0.1,
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
